6906d6ac7e888d2c909cf363686146d4a90600f3
MiguelLee <34088912+MinkyMiguelLee@users.noreply.github.com>
2024-01-23 15:26:01 +0900

    Time: 866 ms, Memory: 61.9 MB - LeetHub

--- rotate-array/rotate-array.ts
+++ rotate-array/rotate-array.ts
@@ -2,9 +2,10 @@
  Do not return anything, modify nums in-place instead.
  */
 function rotate(nums: number[], k: number): void {
-    while(k > 0){
+    let rotNum = k > nums.length ? k % nums.length : k;
+    while(rotNum > 0){
         nums.unshift(nums[nums.length - 1]);
         nums.pop();
-        k--;
+        rotNum--;
     }        
 };
