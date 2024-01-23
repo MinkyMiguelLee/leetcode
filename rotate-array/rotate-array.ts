a53e5a8bbdd68eb8a24f7aaa36c9d6aafcedc7f4
MiguelLee <34088912+MinkyMiguelLee@users.noreply.github.com>
2024-01-23 15:21:02 +0900

    Time: 868 ms, Memory: 62.5 MB - LeetHub

--- rotate-array/rotate-array.ts
+++ rotate-array/rotate-array.ts
@@ -2,19 +2,6 @@
  Do not return anything, modify nums in-place instead.
  */
 function rotate(nums: number[], k: number): void {
-    // // 뒤에서부터 k번째 element가 0번이 됨.
-    // if(nums.length > 1 && k > 0){
-    //     let startIdx = nums.length >= k ? nums.length - k : nums.length - k % nums.length;
-    //     const tmpArr = [...nums];
-    //     nums.forEach((ele: number, idx: number) => {
-    //         nums[idx] = tmpArr[startIdx];
-    //         if(startIdx < nums.length - 1){
-    //             startIdx += 1;
-    //         }else{
-    //             startIdx = 0;
-    //         }
-    //     })
-    // }
     while(k > 0){
         nums.unshift(nums[nums.length - 1]);
         nums.pop();
